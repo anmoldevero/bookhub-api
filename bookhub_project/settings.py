88904291ecc978a13bcab7510703a3f31572cd68
AnmolDevero <anmoldevero974@gmail.com>
2025-09-06 08:05:58 +0500

    Fixed filename spelling: urls.py

--- bookhub_project/settings.py
+++ bookhub_project/settings.py
@@ -155,3 +155,13 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME':timedelta(days=1)
 }
+
+
+
+# Added manually
+
+import os 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- bookhub_project/urls.py
+++ bookhub_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenBlacklistView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
     path('api/logout/', TokenBlacklistView.as_view(), name='logout_view')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
